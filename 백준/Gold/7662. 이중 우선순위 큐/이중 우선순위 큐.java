c994e2c74a632a99ed9ed0ef02b33288cee66c4d
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-03-05 15:37:54 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 3600 ms, Memory: 353872 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,69 +1,62 @@
 import java.io.*;
 import java.util.*;
-import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Element> minQueue = new PriorityQueue<>(Comparator.comparing(Element::getValue));
-        PriorityQueue<Element> maxQueue = new PriorityQueue<>(Comparator.comparing(Element::getValue).reversed());
-        List<Element> elements = new ArrayList<>();
-
-        Map<String, Function<Integer, Integer>> funcs = new HashMap<>();
-        funcs.put("I", (Integer v) -> {
-            Element el = new Element(v);
-            elements.add(el);
-            minQueue.add(el);
-            maxQueue.add(el);
-            return 0;
-        });
-        funcs.put("D", (Integer v) -> {
-            if (v == 1) {
-                if (maxQueue.isEmpty())
-                    return -1;
-                Element el = maxQueue.remove();
-                while (!maxQueue.isEmpty() && el.isRemoved()) {
-                    el = maxQueue.remove();
-                }
-                el.setRemoved(true);
-            } else {
-                if (minQueue.isEmpty())
-                    return -1;
-                Element el = minQueue.remove();
-                while (!minQueue.isEmpty() && el.isRemoved()) {
-                    el = minQueue.remove();
-                }
-                el.setRemoved(true);
-            }
-            return 0;
-        });
-
         while (tc-- > 0) {
             int k = Integer.parseInt(br.readLine());
 
-            minQueue.clear();
-            maxQueue.clear();
-            elements.clear();
+            MinHeap minQueue = new MinHeap(k);
+            MaxHeap maxQueue = new MaxHeap(k);
+            List<Element> elements = new ArrayList<>();
 
             for (int i = 0; i < k; i++) {
                 StringTokenizer tok = new StringTokenizer(br.readLine());
                 String command = tok.nextToken();
                 int n = Integer.parseInt(tok.nextToken());
-                funcs.get(command).apply(n);
+
+                if (command.equals("D")) {
+                    if (n == 1) {
+                        if (maxQueue.isEmpty())
+                            continue;
+                        Element el = maxQueue.remove();
+                        while (!maxQueue.isEmpty() && el.isRemoved()) {
+                            el = maxQueue.remove();
+                        }
+                        el.setRemoved(true);
+                    } else {
+                        if (minQueue.isEmpty())
+                            continue;
+                        Element el = minQueue.remove();
+                        while (!minQueue.isEmpty() && el.isRemoved()) {
+                            el = minQueue.remove();
+                        }
+                        el.setRemoved(true);
+                    }
+                } else {
+                    Element el = new Element(n);
+                    elements.add(el);
+                    minQueue.add(el);
+                    maxQueue.add(el);
+                }
             }
             elements.sort(Comparator.comparing(Element::getValue));
 
             Element min = null;
-            for (Element element : elements) {
+            int z = 0;
+            for (int i = 0; i < elements.size(); i++) {
+                Element element = elements.get(i);
                 if (!element.isRemoved()) {
                     min = element;
+                    z = i;
                     break;
                 }
             }
             Element max = null;
-            for (int i = elements.size() - 1; i >= 0; i--) {
+            for (int i = elements.size() - 1; i >= z; i--) {
                 Element element = elements.get(i);
                 if (!element.isRemoved()) {
                     max = element;
@@ -102,5 +95,159 @@ public class Main {
         public void setValue(int value) {
             this.value = value;
         }
+    }
+
+
+    static class MinHeap {
+        private Element[] tree;
+        private int size;
+        public MinHeap(int length) {
+            tree = new Element[length];
+            size = 0;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public void add(Element value) {
+            tree[size] = value;
+
+            int current = size;
+            size++;
+            while (current > 0) {
+                int parent = getParent(current);
+                if (tree[parent].value > tree[current].value) {
+                    swap(parent, current);
+                    current = parent;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        public Element remove() {
+            Element value = tree[0];
+            tree[0] = tree[--size];
+
+            int current = 0;
+
+            while (true) {
+                int c1 = firstChild(current);
+                int c2 = c1 + 1;
+
+                int sc = current;
+                if (c1 < size && tree[current].value > tree[c1].value) {
+                    sc = c1;
+                }
+                if (c2 < size && tree[sc].value > tree[c2].value) {
+                    sc = c2;
+                }
+
+                if (sc == current) {
+                    break;
+                }
+                swap(current, sc);
+                current = sc;
+            }
+
+            return value;
+        }
+
+        private int getParent(int idx) {
+            return (idx - 1) / 2;
+        }
+
+        private void swap(int a, int b) {
+            Element temp = tree[a];
+            tree[a] = tree[b];
+            tree[b] = temp;
+        }
+
+        private int firstChild(int idx) {
+            return idx * 2 + 1;
+        }
+
+    }
+
+
+    static class MaxHeap {
+        private Element[] tree;
+        private int size;
+        public MaxHeap(int length) {
+            tree = new Element[length];
+            size = 0;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        public void add(Element value) {
+            tree[size] = value;
+
+            int current = size;
+            size++;
+            while (current > 0) {
+                int parent = getParent(current);
+                if (tree[parent].value < tree[current].value) {
+                    swap(parent, current);
+                    current = parent;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        public Element remove() {
+            Element value = tree[0];
+            tree[0] = tree[--size];
+
+            int current = 0;
+
+            while (true) {
+                int c1 = firstChild(current);
+                int c2 = c1 + 1;
+
+                int sc = current;
+                if (c1 < size && tree[current].value < tree[c1].value) {
+                    sc = c1;
+                }
+                if (c2 < size && tree[sc].value < tree[c2].value) {
+                    sc = c2;
+                }
+
+                if (sc == current) {
+                    break;
+                }
+                swap(current, sc);
+                current = sc;
+            }
+
+            return value;
+        }
+
+        private int getParent(int idx) {
+            return (idx - 1) / 2;
+        }
+
+        private void swap(int a, int b) {
+            Element temp = tree[a];
+            tree[a] = tree[b];
+            tree[b] = temp;
+        }
+
+        private int firstChild(int idx) {
+            return idx * 2 + 1;
+        }
+
     }
 }
