c41616f361e90ebea898b42c01b5a209263d240c
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-03-08 14:05:52 +0900

    [Silver I] Title: 단지번호붙이기, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,53 +8,46 @@ public class Main {
 
         int[][] map = new int[n][n];
 
-        Stack<Point> stack = new Stack<>();
+        Stack<int[]> stack = new Stack<>();
+        List<Integer> arr = new ArrayList<>(n * n);
 
-        int k = 2;
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
             for (int j = 0; j < n; j++) {
                 map[i][j] = str.charAt(j) - '0';
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (map[i][j] == 1) {
-                    stack.push(new Point(j, i, k++));
+                    stack.push(new int[]{j, i});
+
+                    int sum = 0;
+                    while (!stack.isEmpty()) {
+                        int[] p = stack.pop();
+                        if (p[0] < 0 || p[1] < 0 || p[0] >= n || p[1] >= n || map[p[1]][p[0]] == 0)
+                            continue;
+
+                        map[p[1]][p[0]] = 0;
+                        sum++;
+
+                        stack.push(new int[]{p[0] - 1, p[1]});
+                        stack.push(new int[]{p[0] + 1, p[1]});
+                        stack.push(new int[]{p[0], p[1] + 1});
+                        stack.push(new int[]{p[0], p[1] - 1});
+                    }
+                    arr.add(sum);
                 }
             }
         }
 
-        Map<Integer, Integer> dazis = new HashMap<>(k);
-        while (!stack.isEmpty()) {
-            Point p = stack.pop();
-            if (p.x < 0 || p.y < 0 || p.x >= n || p.y >= n || map[p.y][p.x] > 1 || map[p.y][p.x] == 0)
-                continue;
-
-            map[p.y][p.x] = p.num;
-            if (!dazis.containsKey(p.num))
-                dazis.put(p.num, 0);
-            dazis.put(p.num, dazis.get(p.num) + 1);
-
-            stack.push(new Point(p.x + 1, p.y, p.num));
-            stack.push(new Point(p.x - 1, p.y, p.num));
-            stack.push(new Point(p.x, p.y + 1, p.num));
-            stack.push(new Point(p.x, p.y - 1, p.num));
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr.size()).append('\n');
+        arr.sort(Comparator.naturalOrder());
+        for(Integer x : arr) {
+            sb.append(x).append('\n');
         }
+        System.out.println(sb);
 
-        Stream<Integer> results = dazis.values().stream().sorted();
-        System.out.println(dazis.size());
-        Iterator<Integer> iter = results.iterator();
-        while (iter.hasNext()) {
-            System.out.println(iter.next());
-        }
-    }
-
-    static class Point {
-        public int x;
-        public int y;
-        public int num;
-
-        public Point(int x, int y, int num) {
-            this.x = x;
-            this.y = y;
-            this.num = num;
-        }
     }
 }
