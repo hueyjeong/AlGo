4f8df193aa5c298fb59ed7f8297eb406c35837fd
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-03-15 22:13:27 +0900

    [Silver V] Title: 숫자 카드, Time: 620 ms, Memory: 115372 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -2,28 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static final boolean[] arr = new boolean[20000002];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = parseInt(br.readLine());
 
         StringTokenizer tok = new StringTokenizer(br.readLine());
 
-        boolean[] arr = new boolean[20000002];
-
         for (int i = 0; i < n; i++) {
-            int v = Integer.parseInt(tok.nextToken());
+            int v = parseInt(tok.nextToken());
             arr[v + 10000001] = true;
         }
 
         StringBuilder sb = new StringBuilder();
-        int m = Integer.parseInt(br.readLine());
+        int m = parseInt(br.readLine());
         tok = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int v = Integer.parseInt(tok.nextToken());
+            int v = parseInt(tok.nextToken());
             sb.append(arr[v + 10000001] ? '1' : '0').append(' ');
         }
 
         System.out.println(sb);
 
+    }
+
+    public static int parseInt(String s) {
+        boolean negative = false;
+        int i = 0, len = s.length();
+        if (s.charAt(0) == '-') {
+            negative = true;
+            i++;
+        }
+        int result = 0;
+        while (i < len) {
+            int digit = s.charAt(i++) - '0';
+            result *= 10;
+            result -= digit;
+        }
+        return negative ? result : -result;
     }
 }
