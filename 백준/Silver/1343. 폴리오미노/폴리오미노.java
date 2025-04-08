5d124e9a44a34ffd1cbf3e90626e394c0a94fe91
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-04-08 22:07:54 +0900

    [Silver V] Title: 폴리오미노, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        s = s.replaceAll("XXXX", "AAAA");
+        s = s.replaceAll("XX", "BB");
+        if (s.contains("X")) {
+            System.out.println(-1);
+        } else {
+            System.out.println(s);
+        }
+    }
+}
