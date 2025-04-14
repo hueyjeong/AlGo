e0d8def07bc918e3d56b419e475f403ea2dce103
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-04-14 23:24:13 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,18 +9,24 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Long.parseLong(br.readLine());
         }
-
-        Map<Integer, Long> dp = new HashMap<>();
-        dp.put(0, arr[0]);
+        
+        long[] dp = new long[n];
+        dp[0] = arr[0];
 
         for (int i = 1; i < n; i++) {
-            var m2 = dp.getOrDefault(i - 2, 0L);
-            var m3 = dp.getOrDefault(i - 3, 0L);
-            dp.put(i, Math.max(
+            long m2 = 0;
+            if (i - 2 >= 0) {
+                m2 = dp[i - 2];
+            }
+            long m3 = 0;
+            if (i - 3 >= 0) {
+                m3 = dp[i - 3];
+            }
+            dp[i] = Math.max(
                     m2 + arr[i],
                     m3 + arr[i - 1] + arr[i]
-            ));
+            );
         }
-        System.out.println(dp.get(n - 1));
+        System.out.println(dp[n - 1]);
     }
 }
