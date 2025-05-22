1a473ed45415dd87cc6b7fc18e025195292face1
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-05-22 18:08:18 +0900

    [D3] Title: 색상환, Time: 81 ms, Memory: 24,704 KB -BaekjoonHub

--- SWEA/D3/23003. 색상환/색상환.java
+++ SWEA/D3/23003. 색상환/색상환.java
@@ -1,28 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.SortedMap;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
-
 public class Solution {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int tc = Integer.parseInt(br.readLine());
-		SortedMap<String, Integer> map = new TreeMap<>();
-		map.put("red", 0);
-		map.put("orange", 1);
-		map.put("yellow", 2);
-		map.put("green", 3);
-		map.put("blue", 4);
-		map.put("purple", 5);
+		int[] map = new int[255];
+		map['r'] = 0;
+		map['o'] = 1;
+		map['y'] = 2;
+		map['g'] = 3;
+		map['b'] = 4;
+		map['p'] = 5;
 		StringBuilder sb = new StringBuilder();
 		while (tc-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			String a = st.nextToken();
-			String b = st.nextToken();
-			int ia = map.get(a);
-			int ib = map.get(b);
+			char a = st.nextToken().charAt(0);
+			char b = st.nextToken().charAt(0);
+			int ia = map[a];
+			int ib = map[b];
 			
 			if (ia == ib) {
 				sb.append('E');
