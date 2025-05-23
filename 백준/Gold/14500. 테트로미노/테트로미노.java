b945f05ccafd5ec3495b7cf97df80224fda05601
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-05-23 17:13:25 +0900

    [Gold IV] Title: 테트로미노, Time: 520 ms, Memory: 35864 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -2,91 +2,91 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static final int[][][] masks = new int[][][] {
+	private static final boolean[][][] masks = new boolean[][][] {
 		{
-			{1,1,1,1},
+			{true,true,true,true},
 		},
 		{
-			{1},
-			{1},
-			{1},
-			{1}
+			{true},
+			{true},
+			{true},
+			{true}
 		},
 		{
-			{1,1},
-			{1,1}
+			{true,true},
+			{true,true}
 		},
 		{
-			{1,1},
-			{1},
-			{1},
+			{true,true},
+			{true},
+			{true},
 		},
 		{
-			{1,1},
-			{0,1},
-			{0,1},
+			{true,true},
+			{false,true},
+			{false,true},
 		},
 		{
-			{1},
-			{1},
-			{1,1},
+			{true},
+			{true},
+			{true,true},
 		},
 		{
-			{0,1},
-			{0,1},
-			{1,1},
+			{false,true},
+			{false,true},
+			{true,true},
 		},
 		{
-			{1,1,1},
-			{1},
+			{true,true,true},
+			{true},
 		},
 		{
-			{1,1,1},
-			{0,0,1},
+			{true,true,true},
+			{false,false,true},
 		},
 		{
-			{1},
-			{1,1,1},
+			{true},
+			{true,true,true},
 		},
 		{
-			{0,0,1},
-			{1,1,1}
+			{false,false,true},
+			{true,true,true}
 		},
 		{
-			{1},
-			{1,1},
-			{0,1}
+			{true},
+			{true,true},
+			{false,true}
 		},
 		{
-			{0,1},
-			{1,1},
-			{1}
+			{false,true},
+			{true,true},
+			{true}
 		},
 		{
-			{1,1},
-			{0,1,1}
+			{true,true},
+			{false,true,true}
 		},
 		{
-			{0,1,1},
-			{1,1}
+			{false,true,true},
+			{true,true}
 		},
 		{
-			{1},
-			{1,1},
-			{1}
+			{true},
+			{true,true},
+			{true}
 		},
 		{
-			{0,1},
-			{1,1},
-			{0,1}
+			{false,true},
+			{true,true},
+			{false,true}
 		},
 		{
-			{0,1},
-			{1,1,1}
+			{false,true},
+			{true,true,true}
 		},
 		{
-			{1,1,1},
-			{0,1}
+			{true,true,true},
+			{false,true}
 		}
 	};
 	public static void main(String[] args) throws Exception {
@@ -105,12 +105,12 @@ public class Main {
 		int max = Integer.MIN_VALUE;
 		for (int i = 0; i < n; i++) {
 			for(int j = 0; j < m; j++) {
-				for(int[][] mask : masks) {
+				for(boolean[][] mask : masks) {
 					try {
 						int sum = 0;
 						for(int k = 0; k < mask.length; k++) {
 							for(int l = 0; l < mask[k].length; l++) {
-								if (mask[k][l] == 1) {
+								if (mask[k][l]) {
 									sum += p[i + k][j + l];
 								}
 							}
