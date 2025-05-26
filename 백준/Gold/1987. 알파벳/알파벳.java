1c1f8c9103ffa0219e791ce51f0fbdf8db807aca
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-05-26 19:23:26 +0900

    [Gold IV] Title: 알파벳, Time: 1128 ms, Memory: 15984 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,15 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     private static char[][] matrix;
-    private static Visit visit;
     private static int alpha;
     private static int r;
     private static int c;
+    private static int[] visited;
+    private static int[][] iii = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,6 +18,7 @@ public class Main {
         c = Integer.parseInt(st.nextToken());
 
         matrix = new char[r][c];
+        visited = new int[r];
 
         for (int i = 0; i < r; i++) {
             String str = br.readLine();
@@ -25,76 +26,41 @@ public class Main {
                 matrix[i][j] = str.charAt(j);
             }
         }
-        visit = new Visit(new int[r]);
         alpha = 0;
         int max = bar(0, 0, 1);
         System.out.println(max);
     }
 
     static int bar(int x, int y, int n) {
-        alpha = putAlpha(alpha, matrix[y][x]);
-        visit.setVisit(x, y);
+        int a = matrix[y][x] - 'A';
+        alpha |= 1 << a;
+        visited[y] |= (1 << x);
+
+        if (alpha == 0x3FFFFFF) {
+            return n;
+        }
 
         int max = n;
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (i == 0 && j == 0) {
-                    continue;
-                }
-                if (i == j || -i == j) {
-                    continue;
-                }
-                int newX = x + i;
-                int newY = y + j;
-                if (newX < 0 || newY < 0 || newY >= r || newX >= c) {
-                    continue;
-                }
-                if (visit.isVisited(newX, newY)) {
-                    continue;
-                }
-                if (containsAlpha(alpha, matrix[newY][newX])) {
-                    continue;
-                }
-                max = Math.max(max, bar(newX, newY, n + 1));
+        for (int[] ii : iii) {
+            int newX = x + ii[0];
+            int newY = y + ii[1];
+            if (newX < 0 || newY < 0 || newY >= r || newX >= c) {
+                continue;
             }
+            if ((visited[newY] & (1 << newX)) > 0) {
+                continue;
+            }
+            a = matrix[newY][newX] - 'A';
+            if ((alpha & 1 << a) > 0) {
+                continue;
+            }
+            max = Math.max(max, bar(newX, newY, n + 1));
         }
-        visit.unsetVisit(x, y);
-        alpha = deleteAlpha(alpha, matrix[y][x]);
+
+        a = matrix[y][x] - 'A';
+        alpha &= ~(1 << a);
+        visited[y] &= ~(1 << x);
 
         return max;
-    }
-
-    static class Visit {
-        int[] visited;
-        public Visit(int[] visited) {
-            this.visited = Arrays.copyOf(visited, visited.length);
-        }
-
-        public boolean isVisited(int x, int y) {
-            int row = visited[y];
-            return (row & (1 << x)) > 0;
-        }
-
-        public void setVisit(int x, int y) {
-            visited[y] |= (1 << x);
-        }
-        public void unsetVisit(int x, int y) {
-            visited[y] &= ~(1 << x);
-        }
-    }
-
-    public static boolean containsAlpha(int alpha, char c) {
-        int a = c - 'A';
-        return (alpha & 1 << a) > 0;
-    }
-    public static int putAlpha(int alpha, char c) {
-        int a = c - 'A';
-        alpha |= 1 << a;
-        return alpha;
-    }
-    public static int deleteAlpha(int alpha, char c) {
-        int a = c - 'A';
-        alpha &= ~(1 << a);
-        return alpha;
     }
 }
