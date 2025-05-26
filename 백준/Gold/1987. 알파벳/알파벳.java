4874bd5431bd2c570487983e9a2c255d985401aa
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-05-26 18:56:05 +0900

    [Gold IV] Title: 알파벳, Time: 3744 ms, Memory: 300164 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,108 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        char[][] matrix = new char[r][c];
+
+        for (int i = 0; i < r; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = str.charAt(j);
+            }
+        }
+
+        int max = bar(new Foo(new Point(0, 0), new Visit(new int[r]), 0, 1), matrix, r, c);
+        System.out.println(max);
+    }
+
+    static int bar(Foo foo, char[][] matrix, int r, int c) {
+        Point p = foo.position;
+        Visit visit = foo.visit;
+
+        if (visit.isVisited(p.x, p.y)) {
+            return foo.n;
+        }
+        if (foo.containsAlpha(matrix[p.y][p.x])) {
+            return foo.n;
+        }
+        foo.putAlpha(matrix[p.y][p.x]);
+
+        int max = foo.n;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (i == j || -i == j) {
+                    continue;
+                }
+                Point newPoint = new Point(p.x + i, p.y + j);
+                if (newPoint.x < 0 || newPoint.y < 0 || newPoint.y >= r || newPoint.x >= c) {
+                    continue;
+                }
+                if (visit.isVisited(newPoint.x, newPoint.y)) {
+                    continue;
+                }
+                if (foo.containsAlpha(matrix[newPoint.y][newPoint.x])) {
+                    continue;
+                }
+                max = Math.max(max, bar(new Foo(newPoint, new Visit(visit.visited), foo.alpha, foo.n + 1), matrix, r, c));
+            }
+        }
+
+        return max;
+    }
+
+    static class Visit {
+        int[] visited;
+        public Visit(int[] visited) {
+            this.visited = Arrays.copyOf(visited, visited.length);
+        }
+
+        public boolean isVisited(int x, int y) {
+            int row = visited[y];
+            return (row & (1 << x)) > 0;
+        }
+
+        public void visit(int x, int y) {
+            visited[y] |= (1 << x);
+        }
+    }
+
+    static class Foo {
+        Point position;
+        Visit visit;
+        int n;
+        int alpha;
+        public Foo(Point position, Visit visit, int alpha, int n) {
+            this.position = position;
+            this.visit = visit;
+            this.n = n;
+            this.alpha = alpha;
+        }
+
+        public boolean containsAlpha(char c) {
+            int a = c - 'A';
+            return (alpha & 1 << a) > 0;
+        }
+        public void putAlpha(char c) {
+            int a = c - 'A';
+            alpha |= 1 << a;
+        }
+    }
+
+    static class Point {
+        int x;
+        int y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
