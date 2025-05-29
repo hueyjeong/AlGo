d9094c9d4665bf31b8d931fc2f907f225cd1b262
Huey S. Jeong <36020499+hueyjeong@users.noreply.github.com>
2025-05-30 00:59:08 +0900

    [Gold IV] Title: 최단경로, Time: 628 ms, Memory: 108176 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -10,15 +10,17 @@ public class Main {
         int e = Integer.parseInt(st.nextToken());
         int a = Integer.parseInt(br.readLine()) - 1;
 
-        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        List<Path>[] map = new List[n];
+        for (int i = 0; i < n; i++) {
+            map[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken()) - 1;
             int v = Integer.parseInt(st.nextToken()) - 1;
             int w = Integer.parseInt(st.nextToken());
-            map.putIfAbsent(u, new HashMap<>());
-            map.get(u).put(v, Math.min(w, map.get(u).getOrDefault(v, INF)));
+            map[u].add(new Path(v, w));
         }
 
         long[] weights = new long[n];
@@ -26,30 +28,52 @@ public class Main {
         Arrays.fill(weights, INF);
         weights[a] = 0;
 
-        Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x[1]));
+        Queue<Path> pq = new PriorityQueue<>(Comparator.comparingInt(Path::getW));
 
-        pq.add(new int[]{a, 0});
+        pq.add(new Path(a, 0));
         while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int v = cur[0];
-            int w = cur[1];
-            if (visited[v]) {
+            Path cur = pq.poll();
+            if (visited[cur.v]) {
                 continue;
             }
-            visited[v] = true;
 
-            Map<Integer, Integer> vv = map.getOrDefault(v, new HashMap<>());
-            for (int key : vv.keySet()) {
-                int ww = vv.get(key) + w;
-                if (weights[key] > ww) {
-                    weights[key] = ww;
-                    pq.offer(new int[]{key, ww});
+            visited[cur.v] = true;
+
+            for (Path p : map[cur.v]) {
+                int ww = p.w + cur.w;
+                if (weights[p.v] > ww) {
+                    weights[p.v] = ww;
+                    pq.offer(new Path(p.v, ww));
                 }
             }
         }
-
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            System.out.println(weights[i] == INF ? "INF" : weights[i]);
+            if (weights[i] == INF) {
+                sb.append("INF");
+            } else {
+                sb.append(weights[i]);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    static class Path {
+        int v;
+        int w;
+        public Path(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        public int getV() {
+            return v;
+        }
+
+        public int getW() {
+            return w;
         }
     }
+
 }
